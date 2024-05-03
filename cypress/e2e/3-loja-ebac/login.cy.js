8ec237f14ad280d173f4e6010e95559010d72ea9
Adailton Alves da Silva <adailton.silva@cardway.com.br>
2024-05-03 15:45:30 -0300

    novos cenarios

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,36 @@
 ///<reference types="cypress"/>
 
-describe('Funcionalidade: Login', () =>{
 
-it('Deve fazer login com sucesso', () => {
+describe('', () => {});
+
+beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-    cy.get('#username').type('adailton.vini.silva@gmail.com')
-    cy.get('#password').type('Eumesmo@11060')
-    cy.get('.woocommerce-form > .button').click()
-    
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, adailton.vini.silva (não é adailton.vini.silva? Sair)')
-    cy.get('a > .hidden-xs').should('contain' , 'Welcome adailton.vini.silva !')
-    
 });
-})
+
+    it('Deve fazer login com sucesso', () => {
+        
+        cy.get('#username').type('adailton.vini.silva@gmail.com')
+        cy.get('#password').type('Eumesmo@11060')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, adailton.vini.silva (não é adailton.vini.silva? Sair)')
+        cy.get('a > .hidden-xs').should('contain' , 'Welcome adailton.vini.silva !')
+    });
+
+    it('Deve exibir uma mensagem de erro ao inseriri usuário invalido', () => {
+        
+        cy.get('#username').type('adailton.kk@gmail.com')
+        cy.get('#password').type('Eumesmo@11060')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'Endereço de e-mail desconhecido')
+        cy.get('.woocommerce-error > li').should('exist')
+    
+    });
+    
+    it('Deve exibir uma mensagem de erro ao inseriri senha invalido', () => {
+        
+        cy.get('#username').type('adailton.vini.silva@gmail.com')
+        cy.get('#password').type('Eumesmo@1106099')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain' , 'Erro: A senha fornecida para')
+        cy.get('.woocommerce-error > li').should('exist')
+});
