1216232c7b693c3c62eae9c9206b515f643c9621
Adailton Alves da Silva <adailton.silva@cardway.com.br>
2024-05-06 13:09:11 -0300

    utilizando lista

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,36 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista - usando primeiro da lista', () => {
+        cy.get('.product-block ')
+            .first()
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    })
+
+    it('Deve selecionar um produto da lista - usando ultimo da lista', () => {
+        cy.get('.product-block ')
+            .last()
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    })
+
+    it('Deve selecionar um produto da lista - escolhendo um da lista', () => {
+        cy.get('.product-block ')
+            .eq(7)
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    })
+
+    it('Deve selecionar um produto da lista - escolhendo um da lista', () => {
+        cy.get('.product-block ')
+            .contains('Aero Daily Fitness Tee')
+            .click()
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
